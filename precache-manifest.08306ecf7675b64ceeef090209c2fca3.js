690de9aeac20cd4a66a806ffd75e20e7b9c68cd4
marison <marison@maven.com.br>
2019-05-27 18:00:58 -0300

    Updates

---
+++ precache-manifest.08306ecf7675b64ceeef090209c2fca3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e367484929743ccb2b17038a23b9f899",
+    "url": "/data-privacy-tools/privacytools.github.io/index.html"
+  },
+  {
+    "revision": "a412a25e70adea55c4f9",
+    "url": "/data-privacy-tools/privacytools.github.io/static/css/4.f0b5ee72.chunk.css"
+  },
+  {
+    "revision": "7009e5241eea40ff93d5",
+    "url": "/data-privacy-tools/privacytools.github.io/static/css/main.42df079f.chunk.css"
+  },
+  {
+    "revision": "b924d21d37ea8aaf0097",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/0.87ccf951.chunk.js"
+  },
+  {
+    "revision": "613e4bf6a9175f77b2e0",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/1.1f2721a3.chunk.js"
+  },
+  {
+    "revision": "1cccead917b00b641d9f",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/10.d2b1afb5.chunk.js"
+  },
+  {
+    "revision": "9884fdcb3e7737e7a071",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/11.641cc6b1.chunk.js"
+  },
+  {
+    "revision": "9d40a5d513acf78ba8c6",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/12.d4a18f06.chunk.js"
+  },
+  {
+    "revision": "728bbf5c5dcf4f208543",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/13.6d39efee.chunk.js"
+  },
+  {
+    "revision": "a412a25e70adea55c4f9",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/4.8661cc01.chunk.js"
+  },
+  {
+    "revision": "a19970c97329c1a89ff0",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/5.7c327b68.chunk.js"
+  },
+  {
+    "revision": "5acda2ceb227ff02742f",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/6.6da18aa5.chunk.js"
+  },
+  {
+    "revision": "9177157581ab0b8e1ae8",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/7.1a1a740b.chunk.js"
+  },
+  {
+    "revision": "3e1c793a2aba7b1569d7",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/8.5349b8da.chunk.js"
+  },
+  {
+    "revision": "f993ce0e452e4d565a74",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/9.96ad9d49.chunk.js"
+  },
+  {
+    "revision": "7009e5241eea40ff93d5",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/main.ce359c0b.chunk.js"
+  },
+  {
+    "revision": "4020c39255d330a79d0f",
+    "url": "/data-privacy-tools/privacytools.github.io/static/js/runtime~main.2fc37be3.js"
+  },
+  {
+    "revision": "dbfaacd99237591c0b41db515f94ae99",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/banner-header.dbfaacd9.svg"
+  },
+  {
+    "revision": "8630b354256614a9725ca1119e3ab98b",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/blockchain-banner.8630b354.svg"
+  },
+  {
+    "revision": "e93102d9e2b204b8106adcf72528593e",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/blockchain-icon.e93102d9.svg"
+  },
+  {
+    "revision": "8ded2a608c58da7db00204867337c5ff",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/confirmedEmail.8ded2a60.svg"
+  },
+  {
+    "revision": "c98009bc5d9c7f966277ac4f4d21da75",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/consentiment-banner.c98009bc.svg"
+  },
+  {
+    "revision": "e0874b029a5d67b0c905693cd6940cf8",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/consentiment-icon.e0874b02.svg"
+  },
+  {
+    "revision": "960ed383e2098397fdbded7e197445c6",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/cookie-banner.960ed383.svg"
+  },
+  {
+    "revision": "5f628fec6f35f82a78612656899ba952",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/cookie-icon.5f628fec.svg"
+  },
+  {
+    "revision": "20b2ef7a5bb73b9bd426b62f7aa9d28f",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/email-icon.20b2ef7a.svg"
+  },
+  {
+    "revision": "7eb368b22c1a353faf536d9066518793",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/logo.7eb368b2.svg"
+  },
+  {
+    "revision": "afc463c36dd307cefde04862d1fa59d0",
+    "url": "/data-privacy-tools/privacytools.github.io/static/media/trubr-logo.afc463c3.svg"
+  }
+]);
