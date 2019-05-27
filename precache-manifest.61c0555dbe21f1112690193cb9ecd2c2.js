3c5a14a0f0fd326fcc667f442e5c6b4f14784481
thommorais <thommorais@gmail.com>
2019-05-27 18:07:53 -0300

    Updates

---
+++ precache-manifest.61c0555dbe21f1112690193cb9ecd2c2.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebbea3e1cea8f642cffd6ba759540622",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c5ac17072541263b2ce",
+    "url": "/static/css/4.17c7817a.chunk.css"
+  },
+  {
+    "revision": "7cc16fbb2bec3ec257de",
+    "url": "/static/css/main.11842c20.chunk.css"
+  },
+  {
+    "revision": "56dc2f9ba90dbb3b1b8b",
+    "url": "/static/js/0.81b503c2.chunk.js"
+  },
+  {
+    "revision": "002f1a40855db712a19a",
+    "url": "/static/js/1.d739f513.chunk.js"
+  },
+  {
+    "revision": "3b478f983f4bda7ade66",
+    "url": "/static/js/10.c164176c.chunk.js"
+  },
+  {
+    "revision": "3ea4d544109602a587c1",
+    "url": "/static/js/11.3a759965.chunk.js"
+  },
+  {
+    "revision": "dda4ebaa07019615a9d2",
+    "url": "/static/js/12.c9d91842.chunk.js"
+  },
+  {
+    "revision": "40674e40bcc25db50851",
+    "url": "/static/js/13.b24c51a5.chunk.js"
+  },
+  {
+    "revision": "3c5ac17072541263b2ce",
+    "url": "/static/js/4.59a0bfab.chunk.js"
+  },
+  {
+    "revision": "5ba6cf59eb51c7a71804",
+    "url": "/static/js/5.3734769f.chunk.js"
+  },
+  {
+    "revision": "0c9b58ea0685ad4d7804",
+    "url": "/static/js/6.3137a6a8.chunk.js"
+  },
+  {
+    "revision": "87c972f41674b35ca50a",
+    "url": "/static/js/7.ae1304da.chunk.js"
+  },
+  {
+    "revision": "b79df34888e666f6e8c8",
+    "url": "/static/js/8.a6989119.chunk.js"
+  },
+  {
+    "revision": "b55ed224222ad4a2ab27",
+    "url": "/static/js/9.84bc1bd1.chunk.js"
+  },
+  {
+    "revision": "7cc16fbb2bec3ec257de",
+    "url": "/static/js/main.40b0d518.chunk.js"
+  },
+  {
+    "revision": "f526269e57ab4822285e",
+    "url": "/static/js/runtime~main.b14637f1.js"
+  },
+  {
+    "revision": "4223c8dee08a5bfb847792d9547d0217",
+    "url": "/static/media/banner-header.4223c8de.svg"
+  },
+  {
+    "revision": "aa2dc89605acc9874930bdfa45d6ab23",
+    "url": "/static/media/blockchain-banner.aa2dc896.svg"
+  },
+  {
+    "revision": "744548ba76f9554c0d77c0c89c18d4f6",
+    "url": "/static/media/blockchain-icon.744548ba.svg"
+  },
+  {
+    "revision": "642017d5c5a80b85634d6e7799b18aba",
+    "url": "/static/media/confirmedEmail.642017d5.svg"
+  },
+  {
+    "revision": "07e49627c566537dacee2f616f614233",
+    "url": "/static/media/consentiment-banner.07e49627.svg"
+  },
+  {
+    "revision": "7698b7ecf6391dee0021d9f64d8516d1",
+    "url": "/static/media/consentiment-icon.7698b7ec.svg"
+  },
+  {
+    "revision": "4e70d95288981178fa96d8e1a937a2df",
+    "url": "/static/media/cookie-banner.4e70d952.svg"
+  },
+  {
+    "revision": "7d8e8bdedaf9d9dfbb49dae454dd90a6",
+    "url": "/static/media/cookie-icon.7d8e8bde.svg"
+  },
+  {
+    "revision": "5c945d27d3d2559dc067a6e755e5454f",
+    "url": "/static/media/email-icon.5c945d27.svg"
+  },
+  {
+    "revision": "0f5ee2ce1490116ed3d8335a61e3e38a",
+    "url": "/static/media/logo.0f5ee2ce.svg"
+  },
+  {
+    "revision": "9b778ecfcaf3ac14aab7d4f8940ced9c",
+    "url": "/static/media/trubr-logo.9b778ecf.svg"
+  }
+]);
