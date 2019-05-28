ff2d63b1616622310310833204cc03028c13faae
marison <marison@maven.com.br>
2019-05-28 18:22:44 -0300

    Envio de email com Mailgun

---
+++ src/components/EmailErro/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { StyledContainer } from './styles.js'
+import { H1 } from '../Title'
+import Paragraph from '../Paragraph'
+import Margin from '../Margin'
+import AppWrapper from '../AppWrapper'
+
+import confirmedEmail from '../../assets/confirmedEmail.svg'
+
+function ConfirmEmail() {
+
+  const { t } = useTranslation()
+
+  return (
+    <AppWrapper>
+      <Margin x={96} />
+      <StyledContainer>
+        <img src={confirmedEmail} alt="E-mail"/>
+        <H1 centered>{t('contact.mail.error.title')}</H1>
+        <Paragraph centered maxWidth={520}>
+          {t('contact.mail.error.headline')}
+        </Paragraph>
+      </StyledContainer>
+      <Margin x={24} />
+    </AppWrapper>
+  )
+}
+
+export default ConfirmEmail
---
+++ src/components/EmailErro/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/macro'
+import {rgba} from 'polished'
+
+export const StyledContainer = styled.div`
+  position: relative;
+  z-index: 2;
+  margin: 0 auto;
+  width: 95%;
+  max-width: 720px;
+  padding: 72px;
+  border-radius: 12px;
+  box-shadow: ${({ theme }) => `0 4px 4px ${rgba(theme.primaryColor, 0.05)}, 0 8px 8px  ${rgba(theme.primaryColor, 0.05)}, 0 16px 16px  ${rgba(theme.primaryColor, 0.05)}, 0 32px 32px  ${rgba(theme.primaryColor, 0.1)}, 0 64px 64px ${rgba(theme.primaryColor, 0.1)}`};
+
+  img{
+    display: block;
+    margin: 0 auto;
+    max-width: 420px;
+  }
+`
