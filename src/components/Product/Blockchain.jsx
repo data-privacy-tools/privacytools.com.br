08fdc29ba46b0de7b42ecda3a9285ec465dbd7b1
thommorais <thommorais@gmail.com>
2019-05-30 02:23:14 -0300

    Fix responsive of the products page

--- src/components/Product/Blockchain.jsx
+++ src/components/Product/Blockchain.jsx
@@ -20,6 +20,7 @@ function Blockchain() {
                 <Row gutter={36} type="flex" align="middle" justify="space-between">
                     <Col xs={24} lg={10}>
                         <img src={blockchainBanner} alt="Blockchain Banner" />
+                        <Margin x={24} />
                     </Col>
                     <Col xs={24} lg={12}>
                         <H1 white>{t('blockchain.title')}</H1>
