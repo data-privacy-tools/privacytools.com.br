a18576b606dd80c85c2746900aee55b1e0740146
thommorais <thommorais@gmail.com>
2019-06-03 03:24:11 -0300

    fixes footer menu on mobile.

--- src/components/Footer/FooterMenu.jsx
+++ src/components/Footer/FooterMenu.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next'
 import { StyledMenu, StyledMenuItem } from './styles'
 
-
 function FooterMenu() {
+	const { t } = useTranslation()
 
-    const { t } = useTranslation()
-
-    return (
-        <StyledMenu>
-            <StyledMenuItem to="/terms-of-use">Terms of Use</StyledMenuItem>
-            <StyledMenuItem to="/privacy-policy">Privacy Policy</StyledMenuItem>
-        </StyledMenu>
-    )
+	return (
+		<StyledMenu>
+			<StyledMenuItem to="/terms-of-use">Terms of Use</StyledMenuItem>
+			<StyledMenuItem to="/privacy-policy">Privacy Policy</StyledMenuItem>
+		</StyledMenu>
+	)
 }
 
 export default FooterMenu
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,29 +2,42 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const StyledFooter = styled.footer`
-    padding: 72px 0 0;
+	padding: 72px 0 0;
 
-    .footer-list{
-        margin-top: 36px;
-        h3{
-            margin-bottom: 24px;
-        }
-        li{
-            margin-top: 6px;
-            a{
-                color: ${props => props.fourthColor};
-            }
-        }
-    }
+	.footer-list {
+		margin-top: 36px;
+		h3 {
+			margin-bottom: 24px;
+		}
+		li {
+			margin-top: 6px;
+			a {
+				color: ${props => props.fourthColor};
+			}
+		}
+	}
 `
 
 export const StyledMenu = styled.div`
-    display: flex;
-    align-items: center;
+	@media (min-width: 701px) {
+		display: flex;
+		align-items: center;
+	}
+
+	@media (max-width: 700px) {
+		display: block;
+	}
 `
 export const StyledMenuItem = styled(Link)`
-    margin: 0 24px;
-    font-weight: 300;
+	font-weight: 300;
+	display: block;
+	@media (min-width: 701px) {
+		margin: 0 24px;
+	}
+
+	@media (max-width: 700px) {
+		margin: 24px 0;
+	}
 `
 
 export default StyledFooter
