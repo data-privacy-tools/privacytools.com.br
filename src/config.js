e4cb5b5f0529d8be03f007abf3b6ff389ab64651
marison <marison@maven.com.br>
2019-06-07 17:31:12 -0300

    captcha v2

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const captcha = `6LeHO6YUAAAAAB77KXogisqUIn4lfz98RNy-h81n`
-export const captcha = `6LdpwacUAAAAAAyumKy5VeU7SlT4fkxbHSGCk7Yy`
+export const captcha = `6LeowacUAAAAAJExZ6gPpU1491Pm3WfnzyFzInFB`
